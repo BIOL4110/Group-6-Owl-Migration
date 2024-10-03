b4aba3320facdb985c890f7faae1b21a7f6506bc
James-Alexander19 <jalexa11@uoguelph.ca>
2024-10-02 20:11:31 -0400

    8:11

--- first_attempt.R
+++ first_attempt.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 
-#the ebird data that I prefiltered only canadian observations (to get file size down)
+#the ebird data that I pre-filtered only Canadian observations (to get file size down)
 Ebird_snowy_owl_Canada
 
 
-#filtering out undeed collums
+#Checking data in breeding areas (to make sure we have enough)
+  # lower Lat of breeding
+  above_60 <-Ebird_snowy_owl_Canada %>% 
+   filter(LATITUDE > 60)
+  #Lower lat of exclusive breeding
+  above_68 <- Ebird_snowy_owl_Canada %>% 
+   filter(LATITUDE > 68)
